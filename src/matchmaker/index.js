3e9a95862e151746c7a2582a182594df0dfb756c
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2023-12-04 12:16:10 +0200

    clients keepalive, matchmaker prunes clients

--- src/matchmaker/index.js
+++ src/matchmaker/index.js
@@ -1,31 +1,40 @@
 // express hello world
 const express = require("express");
 const cors = require("cors");
-const { ExpressPeerServer} = require("peer");
+const { ExpressPeerServer } = require("peer");
 
 const app = express();
 const port = 3000;
 
-const clients = [];
+let clients = {};
+
+setInterval(() => {
+  const len = Object.keys(clients).length;
+  clients = Object.fromEntries(
+    Object.entries(clients).filter(
+      ([_, timestamp]) => new Date().getTime() - timestamp < 20000
+    )
+  );
+  console.log(`Cleared ${len - Object.keys(clients).length} clients`);
+}, 10000);
 
 server = app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
 
 const peerServer = ExpressPeerServer(server, {
-	path: "/",
+  path: "/",
   debug: true,
 });
-
 
 app.use("/peer", peerServer);
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => res.json({ clients }));
+app.get("/", (req, res) => res.json({ clients: Object.keys(clients) }));
 
 app.post("/active", (req, res) => {
-  clients.push(req.body.id);
+  clients[req.body.id] = new Date().getTime();
   res.status(200);
   res.end();
 });
